1edced2877061ebb4c77083de11bdf7947c08eef
Abby Baker <aeb0046@mix.wvu.edu>
2022-01-31 17:46:20 -0500

    fixed top nav and added side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+
 
 import { AppComponent } from './app.component';
+import { SideNavBarComponent } from './navigation/sidenavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    SideNavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
