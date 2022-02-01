483358f1bb747aa49859b1d8c4458e209357b206
Abby Baker <aeb0046@mix.wvu.edu>
2022-02-01 01:13:07 -0500

    added first card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { SignInComponent } from './navigation/signin.component';
-
+import { CategoryCardComponent } from './browse/category.card.component';
 import { AppComponent } from './app.component';
 import { SideNavBarComponent } from './navigation/sidenavbar.component';
 
@@ -13,7 +13,8 @@ import { SideNavBarComponent } from './navigation/sidenavbar.component';
     TopNavBarComponent,
     SideNavBarComponent,
     FooterComponent,
-    SignInComponent
+    SignInComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/browse/category.card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+@Component ({
+    selector: "am-category-card",
+    templateUrl: "category.card.component.html",
+    styleUrls: ["category.card.component.css",]
+})
+
+export class CategoryCardComponent {
+    
+}
